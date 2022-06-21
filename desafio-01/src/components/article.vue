ad42db86cd7bd8f6210005a5c6aff7e68707d484
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-06-20 21:40:29 -0300

    componentes implementado article e header

---
+++ desafio-01/src/components/article.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="card">
+        <div class="header-card">
+            <p class="card-date">{{ date }}</p>
+            <img :src="img" alt="ícone de coração, indicando para salvar artigo">
+        </div>
+        <h4 class="card-title">{{ title }}</h4>
+        <p class="card-description">{{ description }}</p>
+    </div>
+</template>
+<script>
+export default {
+    // name: 'article',
+    props: ['img', 'date', 'title', 'description']
+}
+</script>
+<style>
+h4,p{font-family: 'Lexend Deca', sans-serif;}
+.card{
+    width: -webkit-fill-available;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-items: flex-start;
+    background: var(--white);
+    box-shadow: 0px 0px 10px rgba(19, 19, 31, 0.05);
+    border-radius: 5px;
+    padding: 25px;
+}
+.header-card{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.card-date{
+    color: var(--text-dt-desc);
+    margin-bottom: 22px;
+}
+.card-title{
+    color: var(--text-article-title);
+}
+.card-description{color: var(--text-dt-desc); margin-top: 8px;}
+
+@media only screen and (min-width: 1130px) {
+  .card-date{font-size: 16px;}
+  .header-card >img{
+    width: 22px;
+  }
+  .card-title{font-size: 26px;}
+  .card-description{font-size: 18px;}
+}
+@media only screen and (max-width: 1130px) {
+  .card-date{font-size: 14px;}
+  .header-card >img{
+    width: 19px;
+  }
+  .card-title{font-size: 18px;}
+  .card-description{font-size: 14px;}
+
+}
+</style>
---
+++ desafio-01/src/components/header.vue
@@ -0,0 +1,93 @@
+<template>
+  <header>
+    <div>
+      <div class="bl-title">
+        <span>Codelândia</span>
+        <span>blog</span>
+      </div>
+      <nav>
+        <div class="campo-pesquisa">
+          <img
+            :src="require(`@/assets/Vector.png`)"
+            alt=""
+          />
+          <input type="text" placeholder="Pesquisa no blog" />
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+<style>
+header {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: var(--linear);
+}
+header > div {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+nav {
+  width: 100%;
+}
+.bl-title {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.bl-title > span {
+  color: var(--white);
+  font-family: "Lexend Deca", sans-serif;
+}
+
+.campo-pesquisa {
+  display: flex;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 5px;
+}
+.campo-pesquisa > img {
+  cursor: pointer;
+}
+.campo-pesquisa > input {
+  height: 100%;
+  width: 100%;
+  border: none;
+  background: none;
+  outline: none;
+  font-family: "Inter", sans-serif;
+  color: var(--white);
+  opacity: 0.5;
+}
+.campo-pesquisa > input::-webkit-input-placeholder {
+  color: var(--white);
+  opacity: 0.5;
+}
+
+@media only screen and (min-width: 1130px) {
+  header{  height: 250px;}
+  header > div{  margin: 0 270px;}
+  .bl-title > span{font-size: 24px;}
+  .campo-pesquisa {height: 65px;}
+  .campo-pesquisa > input{font-size: 18px;}
+  .campo-pesquisa > img{width: 23px;padding: 0 20px}
+
+}
+@media only screen and (max-width: 1130px) {
+  header{  height: 200px;}
+  header > div{  margin: 0 20px;}
+  .bl-title > span{font-size: 18px;}
+  .campo-pesquisa {height: 40px;}
+  .campo-pesquisa > input{font-size: 14px;}
+  .campo-pesquisa > img{width: 15px;padding: 0 12px}
+
+
+}
+</style>
