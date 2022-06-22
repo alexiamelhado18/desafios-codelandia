f376d5217b6efa3c5ffa2dd061573cf93b5ecaaf
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-06-22 14:55:13 -0300

    add package scrollreveal and typed

--- desafio-02/src/main.js
+++ desafio-02/src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { VueTypedJs } from 'vue-typed-js'
+import VueScrollReveal from 'vue-scroll-reveal';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(VueTypedJs);
+Vue.use(VueScrollReveal, {
+  origin:'top',
+  distance: '50px',
+  duration: 2000
+});
+
 Vue.config.productionTip = false
 
 new Vue({
